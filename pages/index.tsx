eb2a1d6f0f5bd49ed1b6cb57c1124504408a31a6
turupawn <ahmed.hn.43@gmail.com>
2023-04-25 18:59:48 -0600

    step by step frontend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 
 import initNoirWasm, { acir_read_bytes, compile } from "@noir-lang/noir_wasm";
 import initialiseAztecBackend from "@noir-lang/aztec_backend";
@@ -9,28 +9,22 @@ import { initialiseResolver } from "@noir-lang/noir-source-resolver";
 import { setup_generic_prover_and_verifier } from '@noir-lang/barretenberg';
 
 
-export const compileCircuit = async () => {
+export const compileCircuit = async (noirCode) => {
   await initNoirWasm();
 
-  return await fetch(new URL("../src/main.nr", import.meta.url))
-  .then(r => r.text())
-  .then(code => {
-    initialiseResolver((id : any) => {
-      return code;
-    })
-  })
-  .then(() => {
-    try {
-      const compiled_noir = compile({});
-      return compiled_noir;
-    } catch (e) {
-        console.log("Error while compiling:", e);
-    }
-  })
+  initialiseResolver(() => {
+    return noirCode;
+  });
+  try {
+    const compiled_noir = compile({});
+    return compiled_noir;
+  } catch (e) {
+      console.log("Error while compiling:", e);
+  }
 }
 
-export const getAcir = async () => {
-    const { circuit, abi } = await compileCircuit();
+export const getAcir = async (noirCodeText) => {
+    const { circuit, abi } = await compileCircuit(noirCodeText);
     await initialiseAztecBackend();
 
     // @ts-ignore
@@ -39,33 +33,76 @@ export const getAcir = async () => {
 }
 
 export default function Page() {
-  const myAsynFunction = async () => {
+  const [x, setX] = useState<string>("");
+  const [y, setY] = useState<string>("");
+  const [noirCodeText, setNoirCodeText] = useState("// src/main.nr\nfn main(x : Field, y : pub Field) {\n    constrain x != y;\n}");
+  const [solidityVerifierText, setSolidityVerifierText] = useState("");
+  const [acir, setACIR] = useState(null);
+  const [proofText, setProofText] = useState("Your proof will appear here");
+
+  const handleNoirCodeTextChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setNoirCodeText(event.target.value);
+  };
+
+  const handleGenerateFromNoirCode = async () => {
+    console.log(noirCodeText);
+
     console.log("Initializing...")
-    const [acir, abi] = await getAcir()
+    const [acirTemp, abi] = await getAcir(noirCodeText)
+    setACIR(acirTemp)
     console.log("Generating prover and verifier...")
-    let [prover, verifier] = await setup_generic_prover_and_verifier(acir);
+    let [prover, verifier] = await setup_generic_prover_and_verifier(acirTemp);
     console.log("Prover:")
     console.log(prover)
     console.log("Verifier:")
     console.log(verifier)
-    
+
+    setSolidityVerifierText(verifier.ethSmartContract);
+  };
+
+  const handleClick = async () => {
     console.log("Generating proof...")
     const worker = new Worker(new URL('../pages/worker.ts', import.meta.url));
     worker.onmessage = (e) => {
-        if (e.data instanceof Error) {
-          console.log("There was an error generating the proof")
-        } else {
-          console.log("The proof was generated")
-        }
+      if (e.data instanceof Error) {
+        console.log("There was an error generating the proof")
+      } else {
+        console.log("The proof was generated")
+        setProofText("0x" + Buffer.from(e.data).toString('hex'))
+      }
     }
-    worker.postMessage({ acir, input: {x: 3, y: 4} });
+    worker.postMessage({ acir, input: {x: parseInt(x), y: parseInt(y)} });
     console.log("Finished async function")
-  }
+  };
 
   return (
     <>
-      <h1>Aztec Noir Minified Example</h1>
-      <button onClick={myAsynFunction}>Calculate proof</button>
+      <h1>Aztec Noir Example</h1>
+      <div>
+        <h2>Step1: Generate a verifier from a Noir circuit</h2>
+        <label>
+            Noir Circuit:
+        </label>
+        <br />
+        <textarea rows={10} cols={50} defaultValue={noirCodeText} onChange={handleNoirCodeTextChange}></textarea>
+        <br />
+        <button onClick={handleGenerateFromNoirCode}>Generate Verifier</button>
+        <h2>Step2: Launch your Solidity Verifier</h2>
+        <textarea rows={10} cols={50} defaultValue={solidityVerifierText}></textarea>
+        <h2>Step3: Generate a proof</h2>
+        <label>
+          X:
+          <input type="text" defaultValue={x} onChange={(e) => setX(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Y:
+          <input type="text" defaultValue={y} onChange={(e) => setY(e.target.value)} />
+        </label>
+        <br />
+        <button onClick={handleClick}>Generate Proof</button>
+        <p>{proofText}</p>
+      </div>
     </>  
   )
 }
